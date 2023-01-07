7d3cee473967e316967980130489aa651885089d
gcarniel <gpcarniel@outlook.com>
2023-01-07 11:44:03 -0300

    componente intro finalizado

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,5 +1,13 @@
-import { DescriptionWrapper, ImageWrapper, IntroContainer } from './styles'
+import {
+  Badge,
+  BadgeContainer,
+  BadgeWrapper,
+  DescriptionWrapper,
+  ImageWrapper,
+  IntroContainer,
+} from './styles'
 import coffee from '../../assets/coffee.svg'
+import { ShoppingCart, Timer, Package, Coffee } from 'phosphor-react'
 
 export function Intro() {
   return (
@@ -13,7 +21,44 @@ export function Intro() {
           </p>
         </div>
 
-        <div></div>
+        <div
+          style={{
+            display: 'flex',
+            marginTop: '4.125rem',
+          }}
+        >
+          <BadgeContainer>
+            <BadgeWrapper>
+              <Badge badgeColor="yellow-dark">
+                <ShoppingCart weight="fill" size={32} />
+              </Badge>
+              <span>Compra simples e segura</span>
+            </BadgeWrapper>
+
+            <BadgeWrapper>
+              <Badge badgeColor="yellow-medium">
+                <Timer weight="fill" size={32} />
+              </Badge>
+              <span>Entrega rápida e rastreada</span>
+            </BadgeWrapper>
+          </BadgeContainer>
+
+          <BadgeContainer>
+            <BadgeWrapper>
+              <Badge badgeColor="base-text">
+                <Package weight="fill" size={32} />
+              </Badge>
+              <span>Embalagem mantém o café intacto</span>
+            </BadgeWrapper>
+
+            <BadgeWrapper>
+              <Badge badgeColor="purple-medium">
+                <Coffee weight="fill" size={32} />
+              </Badge>
+              <span>O café chega fresquinho até você</span>
+            </BadgeWrapper>
+          </BadgeContainer>
+        </div>
       </DescriptionWrapper>
       <ImageWrapper>
         <img src={coffee} alt="" />
--- src/components/Intro/styles.ts
+++ src/components/Intro/styles.ts
@@ -2,26 +2,36 @@ import styled from 'styled-components'
 
 export const IntroContainer = styled.div`
   height: 34rem;
-  font-family: 'Baloo 2';
 
   display: flex;
   justify-content: space-between;
-  padding-top: 5.875rem;
 
-  /* background-color: green; */
+  padding-top: 5.875rem;
+  margin-bottom: 6.75rem;
 `
 
 export const DescriptionWrapper = styled.div`
-  /* width: 50%; */
-
   div {
-    /* width: 408px; */
-    background-color: aliceblue;
+    line-height: 1.3;
+
+    h1 {
+      font-family: 'Baloo 2';
+      font-size: 3rem;
+      font-weight: 800;
+      color: ${(props) => props.theme['base-title']};
+      margin-bottom: 1rem;
+    }
+
+    p {
+      font-family: 'Roboto';
+      font-size: 1.25rem;
+      font-weight: 400;
+      color: ${(props) => props.theme['base-subtitle']};
+    }
   }
 `
 
 export const ImageWrapper = styled.div`
-  /* width: 50%; */
   display: flex;
   justify-content: center;
 
@@ -31,4 +41,40 @@ export const ImageWrapper = styled.div`
   }
 
   /* padding: 5.75rem 10rem; */
+`
+const BADGE_COLORS = {
+  'yellow-dark': 'yellow-dark',
+  'yellow-medium': 'yellow-medium',
+  'purple-medium': 'purple-medium',
+  'base-text': 'base-text',
+} as const
+
+interface BadgeProps {
+  badgeColor: keyof typeof BADGE_COLORS
+}
+
+export const BadgeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.125rem;
+  flex: 1;
+`
+
+export const BadgeWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+`
+
+export const Badge = styled.div<BadgeProps>`
+  line-height: 1.3;
+  height: 2rem;
+  width: 2rem;
+
+  svg {
+    padding: 0.5rem;
+    color: ${(props) => props.theme['base-white']};
+    border-radius: 50%;
+    background-color: ${(props) => props.theme[BADGE_COLORS[props.badgeColor]]};
+  }
 `
